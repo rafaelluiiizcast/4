0337f8a1856f8decda3f40e779f0d26a29f05a84
Rafael Luiz <faelluizrosa91@gmail.com>
2026-08-22 01:32:28 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -47,8 +47,7 @@ const Index = () => {
           {/* CTA Button */}
           <div className="text-center">
             <a
-              href="	
-https://compraonlineseguura.com/c/fdeff75446"
+              href="https://checkoutonline.org.ua/c/573a847faa"
               target="_blank"
               rel="noopener noreferrer"
               className="block bg-[hsl(210,80%,42%)] hover:bg-[hsl(210,80%,36%)] text-primary-foreground font-extrabold py-4 px-4 rounded-xl text-sm whitespace-nowrap transition-colors text-center"
